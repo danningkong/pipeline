e94a34e5d6365bc9b7da8b546c9b65c511c3966b
dkong <danningkong@gmail.com>
2023-03-12 15:37:30 +1100

    update file

--- DDL_POC.py
+++ DDL_POC.py
@@ -1,4 +1,15 @@
 import sqlalchemy as sa
+from sqlalchemy import text
+from sqlalchemy.schema import CreateTable
+from sqlalchemy import  MetaData, Table, Column, Integer, String
+
+engine = sa.create_engine('mssql+pyodbc://DESKTOP-68IH9E2\SQLEXPRESS/API?driver=SQL+Server+Native+Client+11.0')
+
+sql=text('create table dbo.test (test int null)')
+
+with engine.connect().execution_options(isolation_level="AUTOCOMMIT") as con:
+
+    con.execute(sql)
 
 
-engine = sa.create_engine('mssql+pyodbc://DESKTOP-68IH9E2\SQLEXPRESS/API')
+con.close()
